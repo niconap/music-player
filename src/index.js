a49ab5aa9116dded81d95a9c158d0b6b23b4b4a5
Nico Nap <nico.nap@outlook.com>
2020-11-15 10:43:13 +0100

    Add button initialization

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import css from "./style.css";
+
+function Buttons(playButton, nextButton, prevButton) {
+  this.next = document.getElementById(nextButton);
+  this.play = document.getElementById(playButton);
+  this.prev = document.getElementById(prevButton);
+  let obj = this;
+
+  this.initialize = function () {
+    let playState = 0;
+    obj.play.addEventListener("click", function () {
+      if (playState == 0) {
+        obj.play.setAttribute("src", "play.png");
+        playState = 1;
+      } else {
+        obj.play.setAttribute("src", "pause.png");
+        playState = 0;
+      }
+    });
+  };
+}
+
+let buttons = new Buttons("play", "next", "prev");
+buttons.initialize();
